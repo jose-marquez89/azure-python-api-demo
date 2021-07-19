dc6671f9d6b997b5ec32bdf7e201b01bae4e513b
jose-marquez89 <jlmarquez89@gmail.com>
2021-07-19 18:46:20 -0500

    calls api and gets data

---
+++ py_api_db_sample/pipeline.py
@@ -0,0 +1,31 @@
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+base_url = "https://api.nytimes.com/svc/topstories/v2/science.json"
+api_key = os.environ["NYT_KEY"]
+key_param = f"?api-key={api_key}"
+res = requests.get(base_url + key_param)
+
+res.raise_for_status()
+
+ny_data = res.json()
+
+if __name__ == "__main__":
+    print("Data Size:", len(ny_data))
+    print("Keys: ", ny_data.keys())
+
+    results = ny_data["results"]
+    print("Results size: ", len(results))
+
+    result_zero = results[0]
+    print("First Result Keys: ", result_zero.keys())
+
+    for key, value in result_zero.items():
+        print(key, end=": ")
+        print(value)
