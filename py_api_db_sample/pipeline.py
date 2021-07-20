f96a5a89486f59c70dda3e47362b40454ecf5404
jose-marquez89 <jlmarquez89@gmail.com>
2021-07-20 09:34:32 -0500

    adds more temp tests

--- py_api_db_sample/pipeline.py
+++ py_api_db_sample/pipeline.py
@@ -1,24 +1,36 @@
 import requests
 import os
 import json
+import logging
 from dotenv import load_dotenv
 
-# TODO: Morph below into a reusable class or function for use within
+FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+
+# TODO: Morph below into a reuable class or function for use within
 # the Azure function
 
 load_dotenv()
 
 
-base_url = "https://api.nytimes.com/svc/topstories/v2/science.json"
-api_key = os.environ["NYT_KEY"]
-key_param = f"?api-key={api_key}"
-res = requests.get(base_url + key_param)
+def get_data(section='science'):
+    base_url = "https://api.nytimes.com/svc/topstories/v2/science.json"
+    api_key = os.environ["NYT_KEY"]
+    key_param = f"?api-key={api_key}"
+    res = requests.get(base_url + key_param)
+    
+    try:
+        res.raise_for_status()
+    except Exception as err:
+        logging.error(err)
+        return
 
-res.raise_for_status()
+    data = res.json()
 
-ny_data = res.json()
+    return data
 
 if __name__ == "__main__":
+    ny_data = get_data()
     print("Data Size:", len(ny_data))
     print("Keys: ", ny_data.keys())
 
@@ -31,3 +43,14 @@ if __name__ == "__main__":
     for key, value in result_zero.items():
         print(key, end=": ")
         print(value)
+
+    rkeys = result_zero.keys()
+    same_keys = []
+    item_type = []
+
+    for result in results:
+        same_keys.append(result.keys() == rkeys)
+        item_type.append(result['item_type'])
+
+    logging.debug(all(same_keys))
+    logging.debug(all(map(lambda x: x == 'Article', item_type)))
