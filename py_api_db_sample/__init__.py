86717e0098c1384900271c8701d578a70333b30a
jose-marquez89 <jlmarquez89@gmail.com>
2021-07-20 17:10:20 -0500

    changes function to timer trigger

--- py_api_db_sample/__init__.py
+++ py_api_db_sample/__init__.py
@@ -1,24 +1,27 @@
 import logging
+import datetime as dt
 
 import azure.functions as func
+from sqlalchemy.orm import session
+from pipeline import get_data, new_engine, new_session, insert_story
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+def main(timer: func.TimerRequest) -> None:
+    utc_timestamp = dt.datetime.utcnow().replace(
+        tzinfo=dt.timezone.utc).isoformat()
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    engine = new_engine()
+    session = new_session(engine)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    topics = ['arts', 'science', 'us', 'world']
+
+    topic_data = []
+    for topic in topics:
+        data = get_data(section=topic)
+        results = data['results']
+        topic_data.append(results)
+    
+    for result_payload in topic_data:
+        for result in result_payload:
+            insert_story(result, session)
+
