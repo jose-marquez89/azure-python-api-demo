986a0b08e9aa3c8b27672010479bb9eb02adda3a
jose-marquez89 <jlmarquez89@gmail.com>
2021-07-20 12:16:43 -0500

    Adds new tables and removes old code

--- py_api_db_sample/models.py
+++ py_api_db_sample/models.py
@@ -1,72 +1,78 @@
-import os
 import logging
-import urllib
 
-import pyodbc
 from dotenv import load_dotenv
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import relationship
+import sqlalchemy
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.sql.expression import true
+
+from pipeline import new_engine, new_session, get_object_id
 
 FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 Base = declarative_base()
 
-# TODO: create at least two tables
-# Tables will be derived based on the following keys
-
-""" 
-    'section', 'subsection', 'title', 'abstract', 'url', 'uri', 
-    'byline', 'item_type', 'updated_date', 'created_date', 'published_date', 
-    'material_type_facet', 'kicker', 'des_facet', 'org_facet', 
-    'per_facet', 'geo_facet', 'multimedia', 'short_url'
-"""
-
-# TODO: set foreign ids in the article table 
 class Article(Base):
     __tablename__ = 'articles'
 
     id = Column(Integer, primary_key=True)
     title = Column(String(100))
     abstract = Column(String(200))
-    # section_id = Column(ForeignKey(None))
-    # url_group_id = Column(ForeignKey(None))
+    section_id = Column(ForeignKey("sections.id"))
     author_id = Column(ForeignKey("authors.id"))
     updated_date = Column(DateTime)
     created_date = Column(DateTime)
     published_date = Column(DateTime)
-    kicker = Column(DateTime)
-    # multimedia_id = Column(ForeignKey(None))
+    kicker = Column(String(50))
 
-# TODO: create the author table
 class Author(Base):
     __tablename__ = 'authors'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(20))
 
-# TODO: create the section table
-    # this will contain subsection
+class Section(Base):
+    __tablename__ = 'sections'
 
-# TODO: create the category (des_facet) table
+    id = Column(Integer, primary_key=True)
+    name = Column(String(15))
 
-# TODO: create the article category table
-    # article id
-    # category id
-    # many-to-one rel to article
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+
+class ArticleCategory(Base):
+    __tablename__ = 'article_category'
+
+    article = Column(Integer, primary_key=True)
+    category = Column(Integer, primary_key=True)
+
+class Url(Base):
+    __tablename__ = 'urls'
+
+    article_id = Column(Integer, primary_key=True)
+    url = Column(String(500))
+    uri = Column(String(500))
+
+class Multimedia(Base):
+    __tablename__ = 'multimedia'
+
+    article_id = Column(Integer, primary_key=True)
+    url = Column(String(500))
+    caption = Column(String(500))
+    copyright = Column(String(30))
 
 if __name__ == "__main__":
     load_dotenv()
-    # create a conection
-    conn_str_a = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:qbofbi.database.windows.net,1433;Database=python_api_db;Uid=MichaelB;Pwd="
-    conn_str_b = "Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-    password = os.environ['DB_PASSWORD'] + ";"
-    conn_str_c = conn_str_a + password + conn_str_b
-    params = urllib.parse.quote_plus(conn_str_c)
-    conn_str = f"mssql+pyodbc:///?odbc_connect={params}"
-    engine = create_engine(conn_str, echo=True) 
 
-    logging.debug("Connection OK")
-    engine.table_names()
+    engine = new_engine()
+    session = new_session(engine)
+
+    new_artcat = ArticleCategory(article=34, category=56)
+    session.add(new_artcat)
+    session.commit()
+    session.close()
